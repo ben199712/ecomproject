0c5c72d319e55565d6e27785d855ec0d7969430b
ben199712 <orijibenjamin1997@gmail.com>
2024-09-11 08:18:49 +0100

    created order and orderitem models

--- ecom/admin.py
+++ ecom/admin.py
@@ -2,8 +2,28 @@ from django.contrib import admin
 from .models import Product, Category, Cart, CartItem
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'price',
+        'stock',
+        'available',
+        'created',
+        'updated',
+    ]
+    list_editable = ['price', 'stock', 'available',]
+    prepopulated_fields = {'slug': ('name',)}
+    list_per_page = 5
+admin.site.register(Product, ProductAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'slug',
+    ]
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Cart)
 admin.site.register(CartItem)
 
